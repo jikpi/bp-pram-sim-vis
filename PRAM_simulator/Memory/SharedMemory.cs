c469d573b6ea707e4580242c99f3e24f3955bfe8
jikpi <jan.kopidol.st@vsb.cz>
2023-10-01 21:03:34 +0200

    Fix negative memory addressing
    Ex.  (S1 := -10) "[S1] := 1" now throws user error.

--- PRAM_simulator/Memory/SharedMemory.cs
+++ PRAM_simulator/Memory/SharedMemory.cs
@@ -1,4 +1,5 @@
-﻿using PRAM_lib.Memory.Interface;
+﻿using PRAM_lib.Code.CustomExceptions;
+using PRAM_lib.Memory.Interface;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
@@ -11,13 +12,26 @@ namespace PRAM_lib.Memory
     internal class SharedMemory : IMemory
     {
         public ObservableCollection<MemoryCell> Cells { get; set; }
+        public int MaxCellSize { get; set; }
+
         public SharedMemory()
         {
             Cells = new ObservableCollection<MemoryCell>();
+            MaxCellSize = 1000000;
+        }
+
+        public void AddressSanityCheck(int memoryAddress)
+        {
+            if (memoryAddress < 0)
+                throw new LocalException($"Address cannot be negative. Tried to access address at {memoryAddress}");
+            if (memoryAddress >= MaxCellSize)
+                throw new LocalException($"Address is too big. Tried to access {memoryAddress}, but max size is {MaxCellSize}");
         }
 
         public MemoryCell Read(int address)
         {
+            AddressSanityCheck(address);
+
             if (Cells.Count <= address)
             {
                 for (int i = Cells.Count; i <= address; i++)
@@ -31,6 +45,8 @@ namespace PRAM_lib.Memory
 
         public void Write(int address, int value)
         {
+            AddressSanityCheck(address);
+
             if (Cells.Count <= address)
             {
                 for (int i = Cells.Count; i <= address; i++)
