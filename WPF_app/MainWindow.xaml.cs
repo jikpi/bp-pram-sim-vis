63e45882feb7461a809dc99bea85a6cf496e6d0a
jikpi <jan.kopidol.st@vsb.cz>
2023-10-22 12:17:00 +0200

    Minor UI improvements

--- WPF_app/MainWindow.xaml.cs
+++ WPF_app/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using PRAM_simulator;
+using System;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
@@ -22,9 +23,6 @@ namespace WPF_app
             InitializeComponent();
 
             InitializeProgram();
-
-
-
         }
 
         public void InitializeProgram()
@@ -56,6 +54,11 @@ namespace WPF_app
             ResetCodeEditorColor();
         }
 
+        private void DataGridMemory_IndexRow(object sender, DataGridRowEventArgs e)
+        {
+            e.Row.Header = (e.Row.GetIndex()).ToString();
+        }
+
         private void ButtonCompile_Click(object sender, RoutedEventArgs e)
         {
             TextRange textRange = new TextRange(RichTextBoxCode.Document.ContentStart, RichTextBoxCode.Document.ContentEnd);
@@ -72,7 +75,7 @@ namespace WPF_app
             {
                 MessageBox.Show("Compilation failed");
                 MessageBox.Show(DataResources.pram.CompilationErrorMessage);
-                if(DataResources.pram.CompilationErrorLineIndex >= 0)
+                if (DataResources.pram.CompilationErrorLineIndex >= 0)
                 {
                     SetCodeEditorLineIndexColor((int)DataResources.pram.CompilationErrorLineIndex);
                 }
@@ -159,34 +162,9 @@ namespace WPF_app
         {
             RichTextBoxCode.Document.Blocks.Clear();
 
-            string[] instructions = {
-                "S0 := READ()",
-                "S1 := READ()",
-                "S2 := S0 + S1",
-                "S3 := S0 - S1",
-                "S4 := S0 * S1",
-                "S5 := S0 / S1",
-                "S6 := S0 % S1",
-                "S7 := [S2]",
-                "S8 := S0 + 2",
-                "S9 := S0 - 2",
-                "S10 := S0 * 2",
-                "S11 := S0 / 2",
-                "S12 := S0 % 2",
-                "[S13] := 10",
-                ":test",
-                "S14 := S14 + 1",
-                "if (S0 == S1) goto :true",
-                "S5 := 0",
-                "goto :end",
-                ":true",
-                "S5 := 1",
-                "goto :end",
-                ":end",
-                "goto :test"
-            };
+            string program = "#Nastavit 5 jako vstup\r\n#Cteni\r\nS0 := READ()\r\n#Prirazeni\r\nS1 := S0\r\n#Prirazeni hodnoty a operace\r\nS2 := S1 + 3\r\nS3 := S1 - 3\r\nS4 := S1 * 3\r\nS5 := S1 / 3\r\nS6 := S1 % 3\r\n\r\n#Prirazeni hodnoty naopak\r\nS7 := 3 + S7\r\nS8 := 3 - S7\r\nS9 := 3 * S7\r\nS10 := 3 / S7\r\nS11 := 3 % S7\r\n\r\n#Prirazeni hodnoty jako vysledek 2 bunek\r\nS12 := S0 + S1\r\nS13 := S0 - S1\r\nS14 := S0 * S1\r\nS15 := S0 / S1\r\nS16 := S0 % S1\r\n\r\n#Pointery\r\nS17 := [S2]\r\n[S17] := 1\r\n[S18] := S17\r\n\r\n#Skoky\r\ngoto :skok1\r\nS19 := S19 / 0\r\n:skok1\r\nif (S0 == S8) goto :skoks2\r\nS19 := S19 / 0\r\n:skoks2\r\n\r\nif (S0 != -191919) goto :skok2\r\nS19 := S19 / 0\r\n:skok2\r\nif (S0 != -1) goto :skok3\r\nS19 := S19 / 0\r\n:skok3\r\n\r\nS20 := 5\r\nS21 := 6\r\nif (S20 >= S21) goto :skok4\r\ngoto :skok5\r\n:skok4\r\nS0 := S0 / 0\r\n:skok5\r\n\r\nif (S21 <= S20) goto :skok6\r\ngoto :skok7\r\n:skok6\r\nS0 := S0 / 0\r\n:skok7\r\n\r\nif (S20 > S21) goto :skok8\r\ngoto :skok9\r\n:skok8\r\nS0 := S0 / 0\r\n:skok9\r\n\r\nif (S21 < S20) goto :skok10\r\ngoto :skok11\r\n:skok10\r\nS0 := S0 / 0\r\n:skok11\r\n\r\n:true\r\nS5 := 1\r\ngoto :end\r\n:end\r\nWRITE(0)";
 
-            foreach (string instruction in instructions)
+            foreach (var instruction in program.Split(new string[] { "\r\n" }, StringSplitOptions.None).ToList())
             {
                 Paragraph paragraph = new Paragraph(new Run(instruction));
                 RichTextBoxCode.Document.Blocks.Add(paragraph);
