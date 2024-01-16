20c44536b7e1cade0bd8db892b2fdff6e4420fcf
jikpi <jan.kopidol.st@vsb.cz>
2024-01-16 17:11:25 +0100

    Added a notification text
    Notifies of the current state of the machine

---
+++ Blazor_app/Services/GlobalService.cs
@@ -0,0 +1,21 @@
+﻿namespace Blazor_app.Services
+{
+    public class GlobalService
+    {
+        //## State text
+        public string LastState { get; private set; } = "None";
+        public enum LastStateUniform { Ok, Note, Error }
+        public LastStateUniform LastStateType { get; private set; } = LastStateUniform.Ok;
+
+        public event Action LastStateUpdated;
+        public void SetLastState(string state, LastStateUniform type)
+        {
+            LastState = state;
+            LastStateType = type;
+            LastStateUpdated?.Invoke();
+        }
+        //---------------------------------------------
+
+        
+    }
+}
