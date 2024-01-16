986a5783106a0fe0945a83af578942187b7d8a2e
jikpi <jan.kopidol.st@vsb.cz>
2024-01-16 18:22:28 +0100

    Adjusted page design
    Added test code, refined notification bar

--- Blazor_app/Services/CodeEditorService.cs
+++ Blazor_app/Services/CodeEditorService.cs
@@ -4,10 +4,17 @@
     {
         public string Code { get; set; } = "";
         public event Action SaveCode;
+        public event Action <string> CodeChanged;
 
         public void Save()
         {
             SaveCode?.Invoke();
+        }
+
+        public void ChangeCode(string code)
+        {
+            Code = code;
+            CodeChanged?.Invoke(code);
         }
     }
 }
